5e1a151534105348f3c0d15d67808946e4f299c7
gtegos <gtegos@iris.gr>
2018-02-06 14:30:40 +0200

    comments on REST API

--- rest.py
+++ rest.py
@@ -5,12 +5,18 @@ from models import RiskTypeDefinition
 
 @server.route('/risktypes', methods=['GET'])
 def RiskTypesGet():
+    ''' Return a list of defined risk types
+        together with their field definitions
+    '''
     risktypes = RiskTypeDefinition.query.order_by(RiskTypeDefinition.name).all()
     return jsonify({'data': [x.serialize for x in risktypes]})
 
 
 @server.route('/risktypes/<risktype>', methods=['GET'])
 def RiskTypeGet(risktype):
+    ''' Return a specific risk type definition
+        together with it's field definitions
+    '''
     risktype = RiskTypeDefinition.query.filter_by(name=risktype).first()
     return jsonify({'data': risktype.serialize})
 
